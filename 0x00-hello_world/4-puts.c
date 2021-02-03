67f06944cdf3379cd9a0d1403bb845261ba724be
Juand0145 <juand0145@gmail.com>
2021-02-03 21:57:55 +0000

    correcting task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 B#include <stdio.h>
 
-/*
-* Write a C program that prints exactly "Programming is like building a multili
-* ngual puzzle, followed by a new line.
-*/
+ /**
+ *main prints Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
