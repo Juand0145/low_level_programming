c610701cb90691ef98adb7e6bd590f783e748c1a
Juand0145 <juand0145@gmail.com>
2021-02-03 21:53:31 +0000

    adding task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+B#include <stdio.h>
 
-/**
- * Write a C program that prints exactly "Programming is like building a multili-
- * ngual puzzle, followed by a new line.
- */
-int main (void)
+/*
+* Write a C program that prints exactly "Programming is like building a multili
+* ngual puzzle, followed by a new line.
+*/
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
