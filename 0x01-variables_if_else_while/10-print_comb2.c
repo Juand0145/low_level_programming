d726ea99702ba043c1b57bdeb550742ce3b2815b
Juand0145 <juand0145@gmail.com>
2021-02-04 23:41:01 +0000

    solving problem to task 6,8,9 and 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n < 10 ; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		for (m = 0; m < 10; m++)
+		for (m = 48; m <= 57; m++)
 		{
 			putchar(n);
 			putchar(m);
-			if (n != 9 || m != 9)
+			if (n != 57 || m != 57)
 			{
 				putchar(',');
 				putchar(' ');
