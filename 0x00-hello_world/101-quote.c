ca6ace7f2884b760c3f1776da4cd262499267d25
Juand0145 <juand0145@gmail.com>
2021-02-04 01:16:46 +0000

    Adding task 8 and 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Write a sentence
+ * Return: In this case we will return 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}
