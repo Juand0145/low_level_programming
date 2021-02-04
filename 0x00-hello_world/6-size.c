db026cff9058add9c320d40e103ab753343f2da8
Juand0145 <juand0145@gmail.com>
2021-02-04 00:28:34 +0000

    adding task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - This main print the diferents bytes size
+* Return: It return the size of diferent numbers
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	pritnf("Size of an int: %d byte (s)\n", sizeof(int));
+	pritnf("Size of a long long int: %d byte(s)\n,", sizeof(long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
