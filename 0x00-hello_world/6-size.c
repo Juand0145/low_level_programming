83ae3472fbfef4aea66b76e3eb49f92734857287
Juand0145 <juand0145@gmail.com>
2021-02-04 01:02:17 +0000

    solvig proble task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	pritnf("Size of an int: %d byte (s)\n", sizeof(int));
-	pritnf("Size of a long int: %d byte(s)\n,", sizeof(long int));
-	pritnf("Size of a long long int: %d byte(s)\n,", sizeof(long long int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n,", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n," sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
