76e1815000eb3210ee1d17753f1620f4c5ac0382
Juand0145 <juand0145@gmail.com>
2021-02-15 23:01:39 +0000

    Creating directory and adding README.md and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - update the value to 98
+ * @n: the value to change
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this file we will learn the uses of pointer, array and strings
