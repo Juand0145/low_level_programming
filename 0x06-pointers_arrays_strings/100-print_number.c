2e29001451e0e5ff19321e5a23884cb84fd09543
Juand0145 <juand0145@gmail.com>
2021-02-19 01:09:18 +0000

    adding task 9

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_number - print numbers
+ * @n: the number to print
+ */
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+
+}
