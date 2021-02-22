c35ae5c7ce1e94508513faa6c5de42a457ed87c0
Juand0145 <juand0145@gmail.com>
2021-02-22 16:27:21 +0000

    adding task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Returns a pointer to the first
+ * occurrence of the character c in the string s,
+ * or NULL if the character is not found
+ * @s: string s
+ * @c : Character to search
+ * Return: Return the string cuted
+ */
+char *_strchr(char *s, char c)
+{
+	int i, a;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		a = *s++;
+
+		if (a == c)
+			return (s - 1);
+	}
+
+	return (NULL);
+}
