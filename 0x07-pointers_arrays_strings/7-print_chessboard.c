f7caacc670951224531d0392eddc7330970caff2
Juand0145 <juand0145@gmail.com>
2021-02-23 00:12:18 +0000

    adding task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - a function that prints the
+ * chessboard.
+ * @a: The matrix of the chess board
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
+}
