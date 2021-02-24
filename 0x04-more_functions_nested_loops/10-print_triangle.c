32bee9a15ea0e5691f9330ae8f53c35d53764d03
Juand0145 <juand0145@gmail.com>
2021-02-24 18:29:31 +0000

    Creating the directory and adding the README

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,12 +13,10 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= size - i; j++)
+		for (j = 0; j < size - i; j++)
 			_putchar(' ');
-
-		for (j = 1; j <= i; j++)
+		for (j = 0; j < i; j++)
 			_putchar('#');
-
 		_putchar('\n');
 	}
 }
