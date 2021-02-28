3d0dc53429efe4ebb821f0afccfc2cc80538a60d
Juand0145 <juand0145@gmail.com>
2021-02-28 22:33:37 +0000

    adding task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc:The input integer values
+ * @argv: The input string values
+ * Return: If tehre is no problem should return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
