3d76fc103470cf8596ff5294405f4fa4bef8ad20
Juand0145 <juand0145@gmail.com>
2021-02-28 22:11:41 +0000

    adding task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc:The input integer values
+ * @argv: The input string values
+ * Return: If tehre is no problem should return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
