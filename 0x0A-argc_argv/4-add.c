23148689c62de9ba7a0bf7a87eabfb7234385dc0
Juand0145 <juand0145@gmail.com>
2021-03-01 01:36:42 +0000

    changing task 4 and using pointers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 
 /**
- * zero_diff - differentiate the number 0 from a string
- * @s: The string to analise
- * Return: Return 0 if is number and 1 if is not
- */
-int zero_diff(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
-		{
-			return (1);
-		}
-	}
-
-	return (0);
-}
-
-/**
  * main - a program that adds positive numbers.
  * @argc:The input integer values
  * @argv: The input string values
@@ -29,7 +9,7 @@ int zero_diff(char *s)
  */
 int main(int argc, char *argv[])
 {
-	long int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
@@ -38,21 +18,23 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for (i = 2; i < argc; i++)
-	{
-		if (atoi(argv[i]) == 0 && zero_diff(argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
 
 	for (i = 1; i < argc; i++)
 	{
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
 		sum += atoi(argv[i]);
 	}
 
-	printf("%ld\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
