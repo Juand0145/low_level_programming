9b108f9688f0e5c2a7ea9eabbd3dc1a8e40d6aa4
Juand0145 <juand0145@gmail.com>
2021-03-02 22:12:27 +0000

    Addign task 0 and holberton.h file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - a function that creates an array of
+ * chars, and initializes it with a specific char.
+ * @size: The size of the array
+ * @c: the character we want to replicate
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ch;
+
+	if (size == 0)
+		return (NULL);
+
+	ch = (char *) malloc(sizeof(char) * size);
+
+	if (ch == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		ch[i] = c;
+
+	ch[i + 1] = '\0';
+
+	return (ch);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,7 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
