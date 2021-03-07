6e9c74de5e6e6b0f16dea0745cd4c39ba7738762
Juand0145 <juand0145@gmail.com>
2021-03-07 18:51:21 +0000

    correcting task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,30 +12,28 @@
  */
 int main(int argc, char *argv[])
 {
-	long int mul;
+	long int multi;
 	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (!(isdigit(argv[i][j])))
 			{
 				printf("Error\n");
-				return (98);
+				exit(98);
 			}
 		}
 	}
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-
-	mul = atoi(argv[1]) * atoi(argv[2]);
-
-	printf("%ld\n", mul);
-
+	multi = atoi(argv[1]) * atoi(argv[2]);
+	printf("%ld\n", multi);
 	return (0);
+
 }
