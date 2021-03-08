9045491eae2baac152fa6e196034a9daa262e80c
Juand0145 <juand0145@gmail.com>
2021-03-08 20:20:22 +0000

    correcting task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A structure call dog
+ * @name: Name of the Dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
 	char *name;
