6515fff1a3b3e901b15e22564b79ffc073da85a4
Juand0145 <juand0145@gmail.com>
2021-03-08 17:45:11 +0000

    Adding task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ * Return: 0 if there is no problem
+ */
+int main (void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
