5c149c5d7d817f81584705946c7c3c6ebb3357c6
Juand0145 <juand0145@gmail.com>
2021-03-10 22:40:46 +0000

    Adding task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
