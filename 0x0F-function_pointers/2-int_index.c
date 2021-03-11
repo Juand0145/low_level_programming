12ae6c973b40d5c7a4d4b5946ce17c357bc927cb
Juand0145 <juand0145@gmail.com>
2021-03-11 00:06:12 +0000

    Correcting task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,10 +16,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
+	}
 	}
 
 	return (-1);
