259183ee2e7cea185ff9fd46429a0f7f3edd52fd
Juand0145 <juand0145@gmail.com>
2021-03-22 15:55:05 +0000

    Correcting betty in the task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- *
+ * print_listint - a function that prints all the elements of a listint_t list.
+ * @h: The direction of the header node.
+ * Return: The number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
