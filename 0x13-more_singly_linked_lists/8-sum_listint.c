43c0fccaeb37b8dd017a6df8b6e5426715d536d7
Juand0145 <juand0145@gmail.com>
2021-03-23 17:49:40 +0000

    Addig task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
