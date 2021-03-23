e2810e581a92e75f01490a73691575e2fb05f5da
Juand0145 <juand0145@gmail.com>
2021-03-23 17:51:31 +0000

    Correcting betty style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
- *
+ * sum_listint -  a function that returns the sum of all the data (n) of a
+ * listint_t linked list.
+ * @head: The starting node of the list
+ * Return: The sum of all numbers of the node's list
  */
 int sum_listint(listint_t *head)
 {
