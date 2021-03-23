faadc8212050034c2c25790f899f1868343ef242
Juand0145 <juand0145@gmail.com>
2021-03-23 14:55:31 +0000

    Correcting betty style

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 
 /**
- *
+ * add_nodeint - a function that adds a new node at the beginning of a
+ * listint_t list.
+ * @head: the Direction of the head of the list
+ * @n: The number of the node
+ * Return: The new node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
@@ -9,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node = NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
