973967d2690835a7c2f8005c41049583e622d2cd
Juand0145 <juand0145@gmail.com>
2021-05-02 10:26:54 -0500

    Fixing betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,22 +18,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	new_node->n = n;
 	node_a = *h;
 	node_b = *h;
-
 	while (node_b != NULL)
 	{
 		if (i < idx)
@@ -45,18 +40,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_b = node_b->next;
 		counter++;
 	}
-
 	if (idx == counter)
 		return (add_dnodeint_end(h, n));
-
 	if (idx > counter)
 		return (NULL);
-
 	node_b = node_a->prev;
 	node_b->next = new_node;
 	new_node->next = node_a;
 	new_node->prev = node_b;
 	node_a->prev = new_node;
-
 	return (new_node);
 }
