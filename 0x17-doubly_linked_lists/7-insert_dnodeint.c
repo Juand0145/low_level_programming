3f6aad8ecd6531cf59f3d68c13cf66e3e2e35e61
Juand0145 <juand0145@gmail.com>
2021-05-02 09:47:06 -0500

    fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	new_node->n = n;
 	node_a = *h;
 
@@ -34,6 +37,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_a = node_a->next;
 		i++;
 	}
+
+	if (idx > i + 1)
+		return (add_dnodeint_end(h, n));
 
 	node_b = node_a->prev;
 	node_b->next = new_node;
