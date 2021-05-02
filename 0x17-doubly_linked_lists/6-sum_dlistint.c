a1e43ac1065ecb3cfd093898fab9fb480d239f12
Juand0145 <juand0145@gmail.com>
2021-05-02 08:01:59 -0500

    Solving betty issues

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
- *
+ * sum_dlistint - a function that returns the sum of all
+ * the data (n) of a dlistint_t linked list.
+ * @head: A pointer to the head
+ * Return: The sum of the list.
  */
 int sum_dlistint(dlistint_t *head)
 {
