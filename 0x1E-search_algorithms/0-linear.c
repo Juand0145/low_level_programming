558361dea21be6d51c7d3dcce580fad50d69c70d
Juand0145 <juand0145@gmail.com>
2021-08-31 18:50:51 -0500

    Adding task 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+
+/**
+ * @value: number to search
+ * @size: elements i the array
+ * @array: POinter to the first
+ * Return: First index where value is found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; array && i < size; ++i)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
